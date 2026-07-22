90ab7f70f12e96b58e2dcef3bdde092c791356db
Michelle Kibichii <jebetmichelle7@gmail.com>
2026-07-22 07:59:25 +0300

    Configure for Render deployment

--- build.sh
+++ build.sh
@@ -4,3 +4,26 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed_data
+
+python manage.py shell << 'EOF'
+from accounts.models import User
+if not User.objects.filter(username='Admin').exists():
+    u = User.objects.create_superuser(
+        username='Admin',
+        email='jebetmichelle7@gmail.com',
+        password='Michelle@2026',
+        first_name='Michelle',
+        last_name='Kibichii',
+    )
+    u.role = 'admin'
+    u.save()
+    print('Admin created')
+else:
+    u = User.objects.get(username='Admin')
+    u.role = 'admin'
+    u.first_name = 'Michelle'
+    u.last_name = 'Kibichii'
+    u.save()
+    print('Admin updated')
+EOF
