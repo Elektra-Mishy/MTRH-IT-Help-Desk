b86b6aef9523941a0546cfcb7100a17b6b03549c
Michelle Kibichii <jebetmichelle7@gmail.com>
2026-07-24 09:53:14 +0300

    updated the email port

--- mtrh_helpdesk/settings.py
+++ mtrh_helpdesk/settings.py
@@ -111,7 +111,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'jebetmichelle7@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'uyhtfhaqcowxlmeq')
